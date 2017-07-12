6ab5d07e1fbcd69e1d1983caa818580ab8979527
wregula <wojciech.regula@securing.pl>
2017-07-12 16:33:16 +0200

    timeout

--- code-examples/Secure session cookies.rb
+++ code-examples/Secure session cookies.rb
@@ -3,4 +3,4 @@
 # Example:
 
 # Add "secure: true" option to APP_DIR/config/initializers/session_store.rb
-Rails.application.config.session_store :cookie_store, key: 'SESSIONID', secure: true
+Rails.application.config.session_store :cookie_store, key: 'SESSIONID', expire_after: 1.hour, secure: true
---
+++ code-examples/Timeout a session.rb
@@ -0,0 +1,6 @@
+# Secure session cookies
+
+# Example:
+
+# Add "expire_after: 1.hour option to APP_DIR/config/initializers/session_store.rb
+Rails.application.config.session_store :cookie_store, key: 'SESSIONID', secure: true, expire_after: 1.hour
