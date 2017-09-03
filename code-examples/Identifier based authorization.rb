56d800a65db42b70c078d0adc0a03f0cb4eab8aa
Kacper Madej <kacperoza@gmail.com>
2017-09-03 17:28:56 +0800

    Review files using Validation class

--- code-examples/Identifier based authorization.rb
+++ code-examples/Identifier based authorization.rb
@@ -1,31 +1,22 @@
 # Identifier based authorization
 
-# Include the classes of which you want to use objects from
-require_relative "classes"
-
 def identifier_based_authorization(input_parameter, pattern, id_to_auth)
+  validator = Validation.new
 
-    validator = Validation.new
+  # First, we want to filter the filenames for expected values. For this example we use only numeric
+  # Whenever the values are tampered with, we can assume an attacker is trying to inject malicious input.
+  # for more information about validation see "input validations" in the code examples:
+  return false unless validator.numeric?(input_parameter)
 
-    # First, we want to filter the filenames for expected values. For this example we use only numeric
-    # Whenever the values are tampered with, we can assume an attacker is trying to inject malicious input.
-    # for more information about validation see "input validations" in the code examples:
-    unless validator.numeric? input_parameter
-        return false
-    end
+  # Second, we want to whitelist the filenames for expected values, in this example they are,
+  # file1,file2 etc.. for more information about whitelisting see "whitelisting" in the code examples:
+  return false unless check_pattern(input_parameter, %w[file1 file2])
 
-    # Second, we want to whitelist the filenames for expected values, in this example they are,
-    # file1,file2 etc.. for more information about whitelisting see "whitelisting" in the code examples:
-    unless check_pattern(input_parameter, ["file1","file2"])
-        return false
-    end
+  # Whenever you are checking whether a user is restricted to review certain data,
+  # the access restrictions should be processed serverside.
+  user = User.find_by(auth_id: input_parameter)
 
-	# Whenever you are checking whether a user is restricted to review certain data,
-	# the access restrictions should be processed serverside.
-	user_auth_id = User.where("auth_id = ?", input_parameter)
-    if id_to_auth == user_auth_id
-    	return true
-    end
+  return true if id_to_auth == user.auth_id
 
-    false
+  false
 end
--- code-examples/System commands.rb
+++ code-examples/System commands.rb
@@ -1,53 +1,39 @@
 # System commands
 
-
-# Here we include all the necessary classes like whitelisting and validation:
-require "classes.rb"
-
 # Define the whitelist pattern and validation type andz input parameter like:
-# get_files("value1,value2,etc", "alphanummeric", params['filename'], "3")
+# get_files("value1,value2,etc", "alphanumeric", params['filename'], "3")
 class SystemCommands
+  # Ruby implementation of PHP escapeshellcmd written by Prof. Edgar Gonzalez
+  def escape_shell_command(cmd)
+    cmd.gsub(/(["'#&;`|*?~<>^()\[\]{}$\\\x0A\xFF])/) { '' }
+  end
 
+  # Whenever a system command is finished, you should properly sanitize and escape this user input.
+  # System command methods examples are: %x{command}, `command`, system("command")
 
-	# Ruby implementation of PHP escapeshellcmd written by Prof. Edgar Gonzalez
-	def escape_shell_command(cmd)
-		return cmd.gsub(/(["'#&;`|*?~<>^()\[\]{}$\\\x0A\xFF])/) { "" }
-	end
+  # First, we want to filter the filenames for expected values. For this example we use only az/09
+  # Whenever the values are tampered with, we can assume an attacker is trying to inject malicious input.
+  # for more information about validation see "input validations" in the code examples:
+  def exec(param, validation_type)
+    validator = Validation.new
 
+    case validation_type
+    when numeric
+      return false unless validator.numeric?(command)
+    when alphanumeric
+      return false unless validator.alphanumeric?(command)
+    else
+      return false
+    end
 
-	# Whenever a system command is finished, you should properly sanitize and escape this user input.
-	# System command methods examples are: %x{command}, `command`, system("command")
+    return false unless check_pattern(param)
 
-	# First, we want to filter the filenames for expected values. For this example we use only az/09
-	# Whenever the values are tampered with, we can assume an attacker is trying to inject malicious input.
-	# for more information about validation see "input validations" in the code examples:
-	def exec(param, validation_type)
+    # If all went good we include the filename
+    # Even though there is a match we still escape the shellcommand:
+    command = './configure ' + param
+    escaped_command = self.escape_shell_command(command)
 
-		validator = new Validation
-		case validation_type
-		when numeric
-			unless validator.numeric?(command)
-				return false
-			end	
-		when alphanumeric
-			unless validator.alphanumeric?(command)
-				return false
-			end
-		else
-			return false
-		end
-
-		unless check_pattern(param)
-			return false
-		end
-		
-		# If all went good we include the filename
-		# Even though there is a match we still escape the shellcommand:
-		command = "./configure " + param
-		escaped_command = self.escape_shell_command(command)
-
-		#Only after validation do we put the shellcommand into the system() method:
-		system(escaped_command)
-
-	end
+    # Only after validation do we put the shell command into the system() method:
+    system(escaped_command)
+  end
 end
