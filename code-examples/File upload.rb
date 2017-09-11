4ff2945cc59eb837fc31fe4bb38b816ffd1b4f12
Kacper Madej <kacperoza@gmail.com>
2017-09-11 15:39:09 +0800

    Properly format and fix regex in File upload example

--- code-examples/File upload.rb
+++ code-examples/File upload.rb
@@ -9,11 +9,12 @@
 # Now something about security - create strong validation rules in model. For example:
 
 # File: APP_DIR/app/models/photo.rb
-  validates_attachment :image, presence: true, # validates if file is proper image
-  content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },  # whitelist content type
-  size: { in: 0..10.kilobytes } # set maximum size of uploading 
-  validates_attachment_file_name :image, matches: [/^[a-zA-Z0-9/$] # allow only alphanumerical file names
+validates_attachment :image,
+                     presence: true, # validates if file is proper image
+                     content_type: {content_type: %w(image/jpeg image/gif image/png)}, # whitelist content type
+                     size: {in: 0..10.kilobytes} # set maximum size of uploading
+validates_attachment_file_name :image, matches: [/^[a-zA-Z0-9]$/] # allow only alphanumerical file names
 
- # Paperclip logs every upload action by default. If you want to turn logging on explicite add following line of code
- # to your APP_DIR/config/environments/development.rb
- Paperclip.options[:log] = true
+# Paperclip logs every upload action by default. If you want to turn logging on explicite add following line of code
+# to your APP_DIR/config/environments/production.rb
+Paperclip.options[:log] = true
